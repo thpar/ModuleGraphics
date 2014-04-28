ff8670ca7c8748fd37d0e502ac8d42bde53db750
thpar <thpar@psb.ugent.be>
2014-04-28 17:49:26 +0200

    A Gradient element that uses a Colorizer

---
+++ be/ugent/psb/modulegraphics/elements/Gradient.java
@@ -0,0 +1,148 @@
+package be.ugent.psb.modulegraphics.elements;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Element that draws a continuous gradient over a given array of
+ * checkpoints, based on a {@link Colorizer<Double>}
+ * 
+ * At least a start and end point should be given.
+ * 
+ * @author Thomas Van Parys
+ *
+ */
+public class Gradient extends Element{
+
+	private Colorizer<Double> c;
+
+	private List<CheckPoint> checkPoints = new ArrayList<CheckPoint>();
+	
+	private int width = 10;
+	private int height = 1;
+	
+	private boolean dataChanged = false;
+
+	private boolean paintLabels = true;
+	
+	/**
+	 * Value increment per pixel
+	 */
+	private double valueStep;
+	
+	private class CheckPoint{
+		public String label;
+		public double value;
+		public int pixelLocation;
+
+		public CheckPoint(String label, double value){
+			this.label = label;
+			this.value = value;
+		}
+	}
+	
+	public Gradient(Colorizer<Double> c){
+		this.c = c;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+		this.dataChanged = true;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+		this.dataChanged = true;
+	}
+
+	public void addCheckPoint(String label, double value){
+		this.checkPoints.add(new CheckPoint(label, value));
+		this.dataChanged = true;
+	}
+	
+	/**
+	 * Maps the checkpoints on their pixel location.
+	 * 
+	 * @return the increment in value for each pixel
+	 */
+	private void translate(){
+		int pixelWidth = this.width * this.getUnit().width;
+		CheckPoint firstPoint = this.checkPoints.get(0);
+		CheckPoint lastPoint = this.checkPoints.get(this.checkPoints.size()-1);
+		
+		firstPoint.pixelLocation = 0;
+		lastPoint.pixelLocation = pixelWidth;
+		
+		double valueRange = lastPoint.value - firstPoint.value;
+				
+		for (int i = 1; i<checkPoints.size()-1; i++){
+			int lastLoc = checkPoints.get(i-1).pixelLocation;
+			double lastValue = checkPoints.get(i-1).value;
+			
+			double thisValue = checkPoints.get(i).value;
+			
+			double pixels = Math.abs(Math.abs(thisValue) - Math.abs(lastValue))/valueRange * pixelWidth;
+			checkPoints.get(i).pixelLocation =  lastLoc + (int)Math.round(pixels);
+		}
+		
+		this.valueStep = valueRange/pixelWidth;
+	}
+
+	@Override
+	protected Dimension paintElement(Graphics2D g, int xOffset, int yOffset) {
+		if (dataChanged && checkPoints.size()>=2){
+			dataChanged = false;
+			translate();
+		}
+		
+		if (checkPoints.size()>=2){
+			int pixelWidth = this.width * this.getUnit().width;
+			double value = checkPoints.get(0).value;
+			for (int i=0; i<=pixelWidth; i++){
+				g.setColor(c.getColor(value));
+				g.drawLine(xOffset+i, yOffset, xOffset+i, yOffset+this.getUnit().height*height);
+				value+=valueStep;
+			}
+			
+			if (paintLabels){
+				//TODO paint labels
+			}
+		}
+		
+		g.setPaint(Color.BLACK);
+		g.drawRect(xOffset, yOffset, width * this.getUnit().width, height * this.getUnit().height);
+		return this.getRawDimension(g);
+	}
+
+	@Override
+	protected Dimension getRawDimension(Graphics2D g) {
+		if (dataChanged && checkPoints.size()>=2){
+			dataChanged = false;
+			translate();
+		}
+		return new Dimension(width * this.getUnit().width, height * this.getUnit().height);
+	}
+
+	public boolean isPaintLabels() {
+		return paintLabels;
+	}
+
+	public void setPaintLabels(boolean paintLabels) {
+		this.paintLabels = paintLabels;
+	}
+	
+	
+	
+}
