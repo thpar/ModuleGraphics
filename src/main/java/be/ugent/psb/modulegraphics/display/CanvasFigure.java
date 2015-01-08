fd80b2d673aab6af0b68a9474f93ba708e390742
thpar <thpar@psb.ugent.be>
2015-01-08 17:32:19 +0100

    Migrate to Maven build

--- src/main/java/be/ugent/psb/modulegraphics/display/CanvasFigure.java
+++ src/main/java/be/ugent/psb/modulegraphics/display/CanvasFigure.java
@@ -16,10 +16,10 @@ import net.sf.epsgraphics.ColorMode;
 import net.sf.epsgraphics.EpsGraphics;
 import be.ugent.psb.modulegraphics.elements.Canvas;
 
+import com.itextpdf.awt.DefaultFontMapper;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.DefaultFontMapper;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfTemplate;
 import com.itextpdf.text.pdf.PdfWriter;
